98677df9db98df1397948833397f413870534c21
Antondragonslayer1337 <57992645+Antondragonslayer1337@users.noreply.github.com>
2020-05-11 00:15:05 +0200

    6

---
+++ main.js
@@ -0,0 +1,19 @@
+function check(){
+	var question1=document.quiz.question1.value;
+	var question2=document.quiz.question2.value;
+	var question3=document.quiz.question3.value;
+	var correct=0;
+
+	if (question1 == "Monty Python") {
+		correct++;
+	}
+	if (question2 == "Turtle") {
+		correct++;
+	}
+	if (question3 == "Textprogrammering") {
+		correct++;
+	}
+
+	document.getElementById("after_submit").style.visibility="visible";
+	document.getElementById("number_correct").innerHTML="Du fick "+ correct + "/3 rätt.";
+}
