b21e492c4b786cd3ca791711e1a854ee655910ee
Antondragonslayer1337 <57992645+Antondragonslayer1337@users.noreply.github.com>
2020-05-12 19:01:23 +0200

    10

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function check(){
+function check(){ /* Quizet*/
 	var question1=document.quiz.question1.value;
 	var question2=document.quiz.question2.value;
 	var question3=document.quiz.question3.value;
